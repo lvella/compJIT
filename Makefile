347b145ceca1c47ed0790876bae070e88cfcbd9f
Lucas Clemente Vella <lvella@gmail.com>
2016-09-21 19:34:31 -0300

    Versão que não depende de LLVM.
    Nem de Clang nem de Cling. Usa o g++ instalado no sistema.

--- Makefile
+++ Makefile
@@ -1,46 +1,29 @@
 # Define compiler
-CXX := clang++
+CXX := g++
 CF90 := gfortran
 
 # Files to compile
-CPPFILE := executaInput.cpp
-FFILE := intermed.f90
-MFILE := fmain.f90
+SOURCE_FILES := \
+	executaInput.cpp \
+	intermed.f90 \
+	fmain.f90
 
-# Exec
+OBJECT_FILES := $(addsuffix .o, $(basename $(SOURCE_FILES)))
+
 EXE := exec
 
-# Objs 
-CPPOBJ := $(patsubst %.cpp, %.o, $(CPPFILE))
-FOBJ := $(patsubst %.f90, %.o, $(FFILE))
-
-# Interoperability Fortran C++
 FFLAGS := -lgfortran
 
-# LLVM FLAGS
-CXXFLAGS :=  $(shell llvm-config --cxxflags) 
-CXXFLAGS :=  $(patsubst -O3,-O0, $(CXXFLAGS))
+$(EXE): $(OBJECT_FILES)
+	g++ $(OBJECT_FILES) -o $(EXE) -lgfortran -ldl
 
-LLVMLIBS := $(shell llvm-config --libs)
-#LLVMLIBFILES := $(shell llvm-config --libfiles)
+%.o: %.cpp
+	g++ -std=c++11 -c $<
 
-CLANGLIBS := -lclangFrontend -lclangSerialization -lclangDriver -lclangCodeGen -lclangParse -lclangSema -lclangStaticAnalyzerFrontend -lclangStaticAnalyzerCheckers -lclangStaticAnalyzerCore -lclangAnalysis -lclangRewriteFrontend -lclangEdit -lclangAST -lclangLex -lclangBasic -lclangFrontend -lclangCodeGen -lclangParse -lclangBasic -lclangSerialization -lclangDriver -lclangEdit -lclangLex -lclangTooling -lclangAnalysis -lclangAST 
+%.o: %.f90
+	gfortran -c $<
 
-CLINGLIBS := -lclingMetaProcessor -lclingInterpreter -lclingUtils
-
-LD_FLAGS := $(shell llvm-config --ldflags)
-
-#OTHERLIBS := -lrt -ldl -ltinfo -latomic -lz -lm -lpthread
-OTHERLIBS := $(shell llvm-config --system-libs) 
-
-
-all:
-	$(CF90) -c $(FFILE)
-	$(CF90) -c $(MFILE)
-	$(CXX) $(CXXFLAGS) -o $(EXE) $(MFILE) $(CPPFILE) $(LFLAGS) $(CLINGLIBS)	$(CLANGLIBS) $(LLVMLIBS) $(LD_FLAGS) $(OTHERLIBS) $(LLVMLIBFILES) $(FFLAGS)
+.PHONY: clean
 
 clean:
 	rm -f *.o *.mod $(EXE)
-
-run:
-	./$(EXE)
--- executaInput.cpp
+++ executaInput.cpp
@@ -1,46 +1,76 @@
-#include "cling/Interpreter/Interpreter.h"
 #include <iostream>
-#include <fstream>
 #include <streambuf>
 #include <sstream>
 #include <string>
-/** 
+#include <random>
+
+#include <unistd.h>
+#include <dlfcn.h>
+
+/**
 * @param fPath eh o caminho para o arquivo em C que será executado
 * @param len eho tanho do nome do arquivo
 * @param fName é o nome da fuco que se executada
-* @param numDouble.. gama parametros da funcao 
+* @param numDouble.. gama parametros da funcao
 * @param &argV end argV
 * @param "/usr/local" llvm dir
 */
 
+static std::string gen_rand_name()
+{
+	const std::string alphabet("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+	std::uniform_int_distribution<> d(0, alphabet.size()-1);
+	std::mt19937 g(std::random_device{}());
+
+	std::string name("/tmp/");
+	for(unsigned i = 0; i < 30; ++i) {
+		name.push_back(alphabet[d(g)]);
+	}
+
+	return name + ".so";
+}
+
 extern "C"{
-extern void cfunc(char *fPath, int *len, char *fName, double *numDouble, double *a, double *C, double *h, double *y, double *v5, double *v6, double *v7, double *v8, double *v9, int *gama)
+extern void cfunc(char *fPath, int *len, char *fName, double *numDouble, double *a, double *C, double *h, double *y, double *v5, double *v6, double *v7, double *v8, double *v9, int *gama);
+}
+
+void cfunc(char *fPath, int *len, char *fName, double *numDouble, double *a, double *C, double *h, double *y, double *v5, double *v6, double *v7, double *v8, double *v9, int *gama)
 	{
 	static double(*f)(double a, double C, double h,  double y, double v5, double v6, double v7, double v8, double v9, int gama) = nullptr;
 
-//	std::cout<<"Valor do fPath = "<<fPath<<'\n';
-	/* tratamento para final de String Fortran para C */	
-	std::string path;
-	int i;
-	for (i = 0; i< *len; i++)
-		path.push_back(fPath[i]);
+        if(!f){
+		std::string path(fPath, *len);
+		std::string libname = gen_rand_name();
 
-	/* atribuo o codigo do aruivo para a string code */
-	std::ifstream t(path); //arquivo de leitura
-	std::stringstream buffer;
-	buffer << t.rdbuf();
-	std::string code = buffer.str();
+		// Call g++ on the file...
+		{
+			std::stringstream cmd;
+			cmd << "g++ -shared -o " << libname
+				<< " -O3 -fPIC -flto -march=native -mtune=native " << path;
+			int ret = system(cmd.str().c_str());
 
-	const char* const argV = "cling";
-//	cling::Interpreter* gCling = new cling::Interpreter(1,&argV,"/home/julia/clonedr/inst");
-	/* instancia interpretador Cling */
-	cling::Interpreter* gCling = new cling::Interpreter(1,&argV,"/usr/local");
-        if(f==nullptr){
-		std::cout << "Primeira vez da execução: \n";
-		f = (double (*)(double a, double C, double h, double y, double v5, double v6, double v7, double v8, double v9, int gama))  gCling->compileFunction(fName, code);
+			if(ret != 0) {
+				std::cerr << "Compilation error!" << std::endl;
+				return;
+			}
+		}
+
+		// Open the just compiled file
+		void* libhandle = dlopen(libname.c_str(), RTLD_LAZY);
+
+		// Delete it, for it is no longer needed
+		unlink(libname.c_str());
+
+		f = (double (*)(double a, double C, double h, double y, double v5, double v6, double v7, double v8, double v9, int gama)) dlsym(libhandle, fName);
+		if(f) {
+			std::cout << "Function \"" << fName << "\" correctly loaded from file \"" << path << "\"." << std::endl;
+		} else {
+			std::cerr << "Error: Function \"" << fName << "\" not found in file \""
+				<< path << "\"." << std::endl;
+			return;
+		}
 	}
-	
-	*numDouble=f(*a,*C,*h,*y,*v5,*v6,*v7,*v8,*v9,*gama);
-	std::cout<<" Valor de y: "<<*y<<" ";
-	}
+
+	*numDouble = f(*a,*C,*h,*y,*v5,*v6,*v7,*v8,*v9,*gama);
+	std::cout<<" Valor de y: "<< *y <<" ";
 }
